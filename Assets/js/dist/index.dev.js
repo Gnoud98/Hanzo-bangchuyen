d29623aea8b55973a9bb203d56f5ef200501dc7b
Lê Dương <leduong98.uneti@gmail.com>
2021-03-09 14:44:49 +0700

    update news slider

--- Assets/js/dist/index.dev.js
+++ Assets/js/dist/index.dev.js
@@ -174,6 +174,52 @@ var FUNCTION = function () {
     });
   };
 
+  var newsSlider = function newsSlider() {
+    jQuery.fn.liScroll = function (settings) {
+      settings = jQuery.extend({
+        travelocity: 0.02
+      }, settings);
+      return this.each(function () {
+        var $strip = jQuery(this);
+        $strip.addClass("newsticker");
+        var stripHeight = 1;
+        $strip.find("li").each(function (i) {
+          stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+        });
+        var $mask = $strip.wrap("<div class='mask'></div>");
+        var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");
+        var containerHeight = $strip.parent().parent().height(); //a.k.a. 'mask' width 	
+
+        $strip.height(stripHeight);
+        var totalTravel = stripHeight;
+        var defTiming = totalTravel / settings.travelocity; // thanks to Scott Waye		
+
+        function scrollnews(spazio, tempo) {
+          $strip.animate({
+            top: '-=' + spazio
+          }, tempo, "linear", function () {
+            $strip.css("top", containerHeight);
+            scrollnews(totalTravel, defTiming);
+          });
+        }
+
+        scrollnews(totalTravel, defTiming);
+        $strip.hover(function () {
+          jQuery(this).stop();
+        }, function () {
+          var offset = jQuery(this).offset();
+          var residualSpace = offset.top + stripHeight;
+          var residualTime = residualSpace / settings.travelocity;
+          scrollnews(residualSpace, residualTime);
+        });
+      });
+    };
+
+    $(function () {
+      $("ul#ticker01").liScroll();
+    });
+  };
+
   var newUpdateProduct = function newUpdateProduct() {
     $('#owl-carousel1').owlCarousel({
       loop: true,
@@ -191,7 +237,7 @@ var FUNCTION = function () {
           items: 2
         },
         600: {
-          items: 1
+          items: 3
         },
         1000: {
           items: 5
@@ -340,9 +386,9 @@ var FUNCTION = function () {
 
   return {
     _: function _() {
-      home_slider(); //features_news()
-
+      home_slider();
       menu_mb();
+      newsSlider();
       newUpdateProduct();
       view_mode();
       back_top();
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -184,6 +184,45 @@ var zoomImg= function(){
 		});
 }
 
+var newsSlider=function(){
+       jQuery.fn.liScroll = function(settings) {
+        settings = jQuery.extend({
+            travelocity: 0.02
+            }, settings);		
+            return this.each(function(){
+                    var $strip = jQuery(this);
+                    $strip.addClass("newsticker")
+                    var stripHeight = 1;
+                    $strip.find("li").each(function(i){
+                        stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+                    });
+                    var $mask = $strip.wrap("<div class='mask'></div>");
+                    var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+                    var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+                    $strip.height(stripHeight);			
+                    var totalTravel = stripHeight;
+                    var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+                    function scrollnews(spazio, tempo){
+                    $strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
+                    }
+                    scrollnews(totalTravel, defTiming);				
+                    $strip.hover(function(){
+                    jQuery(this).stop();
+                    },
+                    function(){
+                    var offset = jQuery(this).offset();
+                    var residualSpace = offset.top + stripHeight;
+                    var residualTime = residualSpace/settings.travelocity;
+                    scrollnews(residualSpace, residualTime);
+                    });			
+            });	
+    };
+    
+    $(function(){
+        $("ul#ticker01").liScroll();
+    });
+}
+
 var newUpdateProduct=function(){
   $('#owl-carousel1').owlCarousel({
   loop: true,
@@ -203,7 +242,7 @@ var newUpdateProduct=function(){
        items: 2,
     },
     600: {
-      items: 1,
+      items: 3,
     
      
     },
@@ -367,8 +406,8 @@ $('#partner_slider').owlCarousel({
 return {
     _: function () {
           home_slider()
-          //features_news()
           menu_mb()
+          newsSlider()
           newUpdateProduct()
           view_mode()
           back_top()
